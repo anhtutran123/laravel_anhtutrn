0de87bc7540e96ae44514142b82a6c56d554b393
anhtutran123 <43411307+anhtutran123@users.noreply.github.com>
2019-07-18 13:26:04 +0700

    Laravel basic

---
+++ resources/views/hello_world/show.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HELLO BRO</title>
+</head>
+<body>
+<img src="https://static.hltv.org//images/galleries/1693-full/1562952066.9185.jpeg" class="hero-image">
+</body>
+</html>
