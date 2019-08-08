c29b9bbbb5fc9cadcf9ce90b0d0c58cce78e1cf3
anhtutran123 <43411307+anhtutran123@users.noreply.github.com>
2019-08-08 15:16:03 +0700

    Laravel immediate 2

--- app/Facades/HelperFacade.php
+++ app/Facades/HelperFacade.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Facade;
+namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
--- config/app.php
+++ config/app.php
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'HelperFacade' => App\Facade\HelperFacade::class,
+        'HelperFacade' => App\Facades\HelperFacade::class,
 
     ],
 
